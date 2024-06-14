76768ff8c47f24feee0dc9ad021489daee464fbb
Brandon Adame <badamegachuz@gmail.com>
2024-06-13 23:00:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function Products() {
+  const listItems = products.map(product =>
+    <li key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  )
+
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
+function MyButton({count, onClick}) {
+  return (
+    <button onClick={onClick}>
+      clicked {count} times
+    </button>
+  )
+}
+
+
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +56,9 @@ function App() {
         >
           Learn React
         </a>
+        <Products />
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
       </header>
     </div>
   );
